ec5bc7f06ad39f42c32ac32f528ba8e91c924c3f
Alexis von Glasow <vonglasow@gmail.com>
2024-03-20 08:15:43 +0100

    fix: go.sum

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v3"
 )
+
+var data = `
+a: Easy!
+b:
+  c: 2
+  d: [3, 4]
+`
+
+type T struct {
+	A string
+	B struct {
+		RenamedC int   `yaml:"c"`
+		D        []int `yaml:",flow"`
+	}
+}
 
 func main() {
 	fmt.Println("This is a template")
+	t := T{}
+
+	err := yaml.Unmarshal([]byte(data), &t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", t)
 }
